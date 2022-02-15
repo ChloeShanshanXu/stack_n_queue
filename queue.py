6756ea8d0c061810508f888387748f049682b944
ChloeShanshanXu <chloexu@umich.edu>
2022-02-14 22:47:03 -0500

    Update queue.py

--- queue.py
+++ queue.py
@@ -35,4 +35,40 @@ class Queue:
     def is_empty(self):
         return len(self) == 0
 
+    def __len__(self):
+        if self._back_index < self._front_index:
+            return len(self._data) - self._front_index + self._back_index
+        return self._back_index - self._front_index
+
+    def _resize(self):
+        new_data = [None] * len(self._data) * 2
+        new_index = 0
+        for index in range(self._front_index, len(self._data)):
+            new_data[new_index] = self._data[index]
+            new_index += 1
+        for index in range(0, self._front_index):
+            new_data[new_index] = self._data[index]
+            new_index += 1
+        self._data = new_data
+        self._front_index = 0
+        self._back_index = new_index
+
+    def resize_smaller(self):
+        new_data = [None] * (len(self._data)//2)
+        new_index = 0
+        if self._back_index > self._front_index:
+            for index in range(self._front_index, self._back_index):
+                new_data[new_index] =[index]
+                new_index += 1
+        else:
+            for index in range(self._front_index, len(self._data)):
+                new_data[new_index] = self._data[index]
+                new_index += 1
+            for index in range(0, self._back_index):
+                new_data[new_index] = self._data[index]
+                new_index += 1
+
+                
+
+
 
